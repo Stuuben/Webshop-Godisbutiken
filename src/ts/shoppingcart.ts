c2d2d64895b771a58b285d17df6eb576ac82e183
Adam Stuborn <adam_stuborn@hotmail.com>
2022-12-22 16:23:24 +0100

    skapat + och - knappar, gjort så man kam ta bort varor från varukorgen. osv

--- src/ts/shoppingcart.ts
+++ src/ts/shoppingcart.ts
@@ -12,6 +12,7 @@ paymentButton.addEventListener("click", () => {
 let trashcan = document.getElementById("trashcan") as HTMLDivElement;
 trashcan.addEventListener("click", () => {
   candys.length = 0;
+  summary.innerHTML = "0 kr";
   console.log(candys);
   handleShoppinglist();
 });
@@ -25,14 +26,26 @@ function handleShoppinglist() {
 
   for (let i = 0; i < candys.length; i++) {
     let candyItemWrapper = document.createElement("div");
+    let buttonWrapper = document.createElement("div");
     candyItemWrapper.classList.add("itemWrapper");
     let candyName = document.createElement("p");
-
+    candyName.classList.add("candy__name");
     let candyPrice = document.createElement("span");
+    let minusButton = document.createElement("button");
+    minusButton.classList.add("button__minus");
+    let plusButton = document.createElement("button");
+    plusButton.classList.add("button__plus");
+
+    minusButton.addEventListener("click", () => subtrackCandy(i));
+    plusButton.addEventListener("click", () => addCandy(i));
 
     candyName.innerHTML = candys[i].name;
     candyPrice.innerHTML = String(candys[i].price) + " kr";
+    minusButton.innerHTML = "-";
+    plusButton.innerHTML = "+";
 
+    candyItemWrapper.appendChild(minusButton);
+    candyItemWrapper.appendChild(plusButton);
     candyItemWrapper.appendChild(candyName);
     candyName.appendChild(candyPrice);
     shoppingCart.appendChild(candyItemWrapper);
@@ -40,18 +53,35 @@ function handleShoppinglist() {
 }
 handleShoppinglist();
 
+//
+
+// Räknar ut Summan av alla varorna
 let sum = 0;
-
-function addSummary() {
-  let summary = document.getElementById(
-    "item__summary"
-  ) as HTMLParagraphElement;
-
+let summary = document.getElementById("item__summary") as HTMLParagraphElement;
+function handleSummary() {
   for (let i = 0; i < candys.length; i++) {
-    //summary.innerHTML += (candys[i].price += sum).toString();
     sum += candys[i].price;
-    summary.innerHTML = sum.toString();
+    summary.innerHTML = sum.toString() + " kr";
   }
 }
+handleSummary();
 
-addSummary();
+//
+
+//
+function subtrackCandy(i: number) {
+  console.log("click-");
+  console.log(candys[i].price);
+  console.log(sum);
+  candys.splice(i, 1);
+
+  handleShoppinglist();
+  handleSummary();
+}
+
+function addCandy(i) {
+  console.log("click+");
+  candys.push(candys[i]);
+  handleShoppinglist();
+  handleSummary();
+}
