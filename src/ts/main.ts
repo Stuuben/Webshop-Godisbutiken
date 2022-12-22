500bc9ba2e6dd1dd460ff603e6be8881d05f1b5c
Felicia <87656480+felicianorman@users.noreply.github.com>
2022-12-22 09:47:38 +0100

    fixade products knappen

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,23 +4,25 @@ class Candy {
   name: string;
   price: number;
   type: string;
-  constructor(name: string, price: number, type: string) {
+  img: string;
+  constructor(name: string, price: number, type: string, img :string) {
     this.name = name;
     this.price = price;
     this.type = type;
+    this.img = img;
   }
 }
 
-let fizzyBubliz = new Candy("Fizzy Bubliz", 6, "Sur");
-let peanutSquare = new Candy("Jordnötsruta", 5, "Choklad");
-let punsch = new Candy("Punschpralin", 4.5, "Choklad");
-let sourNapps = new Candy("Sura Nappar", 5, "Sur");
-let geisha = new Candy("Geisha", 8, "Choklad");
-let ferrari = new Candy("Ferrari", 5, "Hallon");
-let marabou = new Candy("Marabou", 8, "Choklad");
-let rassberrySkull = new Candy("Hallonlakritsskalle", 5.5, "Hallon");
+let fizzyBubliz = new Candy("Fizzy Bubliz", 6, "Sur", "/fizzybubbliz.2140eb84.png");
+let peanutSquare = new Candy("Jordnötsruta", 5, "Choklad", "/jordnotsruta.0741e7ef.png");
+let punsch = new Candy("Punschpralin", 4.5, "Choklad", "/punschpraliner.9409834c.jpeg");
+let sourNapps = new Candy("Sura Nappar", 5, "Sur", "/suranappar.2ab93ae2.jpg");
+let geisha = new Candy("Geisha", 8, "Choklad", "/geisha.39b044ab.jpg");
+let ferrari = new Candy("Ferrari", 5, "Hallon", "/ferrari.4eefc7a1.png");
+let marabou = new Candy("Marabou", 8, "Choklad", "/marabou.f77e3111.png");
+let rassberrySkull = new Candy("Hallonlakritsskalle", 5.5, "Hallon", "/hallonlakrits.64cd7f44.png");
 
-let candys: Candy[] = [
+export let candys: Candy[] = [
   fizzyBubliz,
   peanutSquare,
   punsch,
