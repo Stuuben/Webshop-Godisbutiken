a42ae9927f34c0a4f65f0f4b5fd7d684f5ec5fa2
Felicia <87656480+felicianorman@users.noreply.github.com>
2022-12-28 13:36:27 +0100

    mer styling

--- src/ts/paymentsite.ts
+++ src/ts/paymentsite.ts
@@ -7,8 +7,10 @@ confirmBuy.addEventListener("click", customerPurchase);
 function customerPurchase() {
   console.log(userName.value);
 
-  let userString = JSON.stringify(userName.value);
-  localStorage.setItem("user", userString);
+  if (userName.value.length > 1) {
+    let userString = JSON.stringify(userName.value);
+    localStorage.setItem("user", userString);
 
-  document.location.href = "./../pages/purchase.html";
+    document.location.href = "./../pages/purchase.html";
+  }
 }
