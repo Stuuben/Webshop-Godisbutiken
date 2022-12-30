1d2a7d1baa2c06eb2624d3916695b3dfadaacb2d
Felicia <87656480+felicianorman@users.noreply.github.com>
2022-12-30 12:21:17 +0100

    fixade bilder

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -42,56 +42,56 @@ let fizzyBubliz = new Candy(
   "Fizzy Bubliz",
   6,
   "Sur",
-  "/fizzybubbliz.2140eb84.png",
+  "https://i1.wp.com/segagubben.se/wp-content/uploads/2018/09/fizzy-pop-losgodis-sega-gubben.jpg?fit=709%2C472&ssl=1",
   "Fizzy Bubblizz är en rosa och blå flaska med en mjuk konsistens och har en smak av bubbelgum."
 );
 let peanutSquare = new Candy(
   "Jordnötsruta",
   5,
   "Choklad",
-  "/jordnotsruta.0741e7ef.png",
+  "https://i2.wp.com/segagubben.se/wp-content/uploads/2018/09/jordnotsruta-losgodis-sega-gubben.jpg?fit=208%2C172&ssl=1",
   "Knapriga jordnötskross i mörk choklad."
 );
 let punsch = new Candy(
   "Punschpralin",
   4.5,
   "Choklad",
-  "/punschpraliner.9409834c.jpeg",
+  "https://www.happycandy.se/images/zoom/punschpraliner.png",
   " Den härliga pralinen har smak av arrak och rom, har ett överdrag av smakrik choklad och är toppad med sockerpärlor."
 );
 let sourNapps = new Candy(
   "Sura Nappar",
   5,
   "Sur",
-  "/suranappar.2ab93ae2.jpg",
+  "https://swedishfoodshop.com/pub/media/catalog/product/cache/577ee1db3aa78a031ff4355fb63b3264/s/u/suracolanappjpg.jpg",
   "Sursockrad gelé med colasmak"
 );
 let geisha = new Candy(
   "Geisha",
   8,
   "Choklad",
-  "/geisha.39b044ab.jpg",
+  "https://swedishfoodshop.com/pub/media/catalog/product/cache/577ee1db3aa78a031ff4355fb63b3264/g/e/geisha_4.jpg",
   "Mjölkchoklad med krispig nötnougat"
 );
 let ferrari = new Candy(
   "Ferrari",
   5,
   "Hallon",
-  "/ferrari.4eefc7a1.png",
+  "https://www.cooperscandy.com/upload/prod/25861.jpg",
   "Röda sega godisar med smak av hallon. Den är formad efter en ferraribil och har en söt smak."
 );
 let marabou = new Candy(
   "Marabou",
   8,
   "Choklad",
-  "/marabou.f77e3111.png",
+  "https://quickbutik.imgix.net/23396w/products/5fd8ff9a2e97e.jpeg",
   "Marabou mjölkchoklad är Sveriges allra mest älskade choklad. Det smidiga mini-formatet gör att det blir lätt att ta med."
 );
 let rassberrySkull = new Candy(
   "Hallonlakritsskalle",
   5.5,
   "Hallon",
-  "/hallonlakrits.64cd7f44.png",
+  "https://bubs.se/wp-content/uploads/2021/08/Hallon_Lakritsskalle.png",
   "Den är en söt och salt dröm med en spännande form."
 );
 
